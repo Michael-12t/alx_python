41465cb0bcfe59862614d3d4d07c53e2bba8dabb
Michael <mic9881@gmail.com>
2023-08-06 14:03:37 -0700

    main

---
+++ python-almost_a_circle/models/base.py, models/models/models/base.py, models/models/models/models/models/models/models/rectangle.py
@@ -0,0 +1,22 @@
+import uuid
+
+
+class Rectangle:
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        if id is not None:
+            self.id = id
+        else:
+            self.id = uuid.uuid4()
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+
+    def display(self):
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
---
+++ python-almost_a_circle/models/base.py, models/models/models/base.py, models/models/models/models/rectangle.py
@@ -0,0 +1,19 @@
+import uuid
+
+
+class Rectangle:
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        if id is not None:
+            self.id = id
+        else:
+            self.id = uuid.uuid4()
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+
+    def area(self):
+        return self.width * self.height
