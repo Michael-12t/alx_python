7ca65eaa05b854e741e22873be8eb2d7b1401b6b
Michael <mic9881@gmail.com>
2023-09-03 04:57:02 -0700

    correction

---
+++ python-web_framework/6-number_odd_or_even.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    # Replace underscore symbols with a space
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    # Replace underscore symbols with a space
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    return f'{n} is a number'
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_route(n):
+    return render_template('5-number.html', n=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even_route(n):
+    if n % 2 == 0:
+        result = 'even'
+    else:
+        result = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, result=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
